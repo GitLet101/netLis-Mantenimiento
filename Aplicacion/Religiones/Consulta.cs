0fc8a698c5d3b095f1d4c2d49d3ff22b9cff0e92
Patrick <48617826+GitLet101@users.noreply.github.com>
2021-11-26 08:49:19 -0600

    Consultas por API

---
+++ Aplicacion/Religiones/Consulta.cs
@@ -0,0 +1,31 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Religiones
+{
+    public class Consulta
+    {
+        public class Ejecuta : IRequest<List<TblCatReligion>> { }
+
+        public class Manejador : IRequestHandler<Ejecuta, List<TblCatReligion>>
+        {
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<TblCatReligion>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var pais = await _context.TblCatReligion.ToListAsync();
+                return pais;
+            }
+        }
+    }
+}
--- Dominio/TblCatReligion.cs
+++ Dominio/TblCatReligion.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dominio
+#nullable disable
+namespace Dominio.Model
 {
-    public class TblCatReligion
+    public partial class TblCatReligion
     {
+        public TblCatReligion()
+        {
+            TblPacientes = new HashSet<TblPaciente>();
+        }
+
         public Guid IdReigion { get; set; }
         public string Descripcion { get; set; }
+
+        public virtual ICollection<TblPaciente> TblPacientes { get; set; }
     }
 }
